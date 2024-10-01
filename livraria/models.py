66299cf32c7b6ca56d7aa2c9602ef38f32e6c39d
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-01 16:14:29 -0300

    feat: tabelas Autor e Livro

--- livraria/models.py
+++ livraria/models.py
@@ -14,4 +14,39 @@ class Editora(models.Model):
     site = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
+    # alterar o nome da classe
+    class Meta:
+        # nome do modelo no singular
+        verbose_name = "Autor"
+        # alterar o nome da classe no plural (o padrão é acrescentar um 's')
+        verbose_name_plural = "Autores"
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    # FKs: 
+    categoria = models.ForeignKey(
+        Categoria, # model associado 
+        on_delete=models.PROTECT, # ->impede de apagar uma categoria que possua livros associados 
+        related_name="livros" # cria um atributo livros na classe[...]
+    )
+    editora = models.ForeignKey(
+        Editora,
+        on_delete=models.PROTECT,
+        related_name="livros"
+    )
+
+    def __str__(self):
+        return f"{self.titulo} ({self.quantidade})"
+    
