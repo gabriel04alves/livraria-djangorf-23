4fff579bb59390d2142bd95352c2553f50e0c200
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-01 15:47:43 -0300

    feat: tabelas Categoria e Editora

--- livraria/models.py
+++ livraria/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model): 
+    descricao = models.CharField(max_length=100)
+    
+    # nome para o campo no admin
+    def __str__(self): # self +- = this
+        return self.descricao
+    
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+                            #nulo,    #pode ser em branco
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
