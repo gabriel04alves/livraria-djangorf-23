dc0c96ca3145b9c0b2662bff6b33558c2bac6379
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-07 18:14:47 -0300

    refactor: exibindo status do carrinho completo no endpoint de compras

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -2,7 +2,12 @@ from rest_framework.serializers import ModelSerializer, CharField
 from livraria.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    
+    # O parâmetro source indica qual campo do model será utilizado para preencher o campo do serializer.
+    # O parâmetro read_only indica que o campo em questão não será utilizado para atualizar a model.
     usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    
     class Meta:
         model = Compra
         fields = "__all__"
