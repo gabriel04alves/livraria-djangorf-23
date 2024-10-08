b2e3b2ffb130beeb3095281b339516df1d38bb97
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 12:14:55 -0300

    feat: incluindo endpoint para criar novas compras

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -31,3 +31,24 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "itens", "total_compra")
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta: 
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+
+    class Meta:
+        model = Compra 
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
+    
