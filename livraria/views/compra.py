72660b367da1994efcab377869047e10223b93aa
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 16:26:34 -0300

    feat: adicionando tipos de usuarios

--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from livraria.models import Compra
+from usuario.models import Usuario
 from livraria.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 class CompraViewSet(ModelViewSet):
@@ -16,5 +17,7 @@ class CompraViewSet(ModelViewSet):
         if usuario.is_superuser:
             return Compra.objects.all()
         if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        if usuario.tipo == Usuario.Tipos.GERENTE:
             return Compra.objects.all()
         return Compra.objects.filter(usuario=usuario)
--- usuario/models.py
+++ usuario/models.py
@@ -6,6 +6,11 @@ from .managers import CustomUserManager
 from uploader.models import Image
 
 class Usuario(AbstractUser):
+    class TipoUsuario(models.IntegerChoices):
+        CLIENTE = 1, "Cliente"
+        VENDEDOR = 2, "Vendedor"
+        GERENTE = 3, "Gerente"
+
     username = None
     email = models.EmailField(_("e-mail address"), unique=True)
     cpf = models.CharField(_("CPF"), max_length=11, blank=True, null=True)
@@ -20,6 +25,8 @@ class Usuario(AbstractUser):
         blank=True,
         default=None
     )
+
+    tipo_usuario = models.IntegerField(choices=TipoUsuario.choices, default=TipoUsuario.CLIENTE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
