610202fe76db2d3f7092614fa52e97a2bda93ccc
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 16:15:58 -0300

    feat: adicionando atributo tipo de pagamento a entidade Compra

--- livraria/models/compra.py
+++ livraria/models/compra.py
@@ -3,6 +3,7 @@ from usuario.models import Usuario
 from .livro import Livro
 
 class Compra (models.Model):
+
     class StatusCompra(models.IntegerChoices):
         CARRINHO = 1, "Carrinho",
         REALIZADO = 2, "Realizado",
@@ -12,6 +13,17 @@ class Compra (models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
     data = models.DateTimeField(auto_now_add=True)
+    
+    class TipoPagamento(models.IntegerChoices):
+        CARTAO_CREDITO = 1, "Cartão de Crédito"
+        CARTAO_DEBITO = 2, "Cartão de Débito"
+        PIX = 3, "PIX"
+        BOLETO = 4, "Boleto"
+        TRANSFERENCIA_BANCARIA = 5, "Transferência Bancária"
+        DINHEIRO = 6, "Dinheiro"
+        OUTRO = 7, "Outro"
+
+    tipo_pagamento = models.IntegerField(choices=TipoPagamento.choices, default=TipoPagamento.CARTAO_CREDITO)
 
     def __str__(self):
         return f"{self.id} | {self.usuario} | {self.status}"
