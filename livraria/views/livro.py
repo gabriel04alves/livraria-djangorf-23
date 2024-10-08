f19db697b3e2480521d705b0f0d71fd47a27f37b
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 15:53:03 -0300

    feat: ordenação de itens

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 from livraria.models import Livro
 from livraria.serializers import LivroSerializer, LivroListSerializer, LivroDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import FilterSet, CharFilter
-
 
 class LivroFilter(FilterSet):
     categoria = CharFilter(field_name="categoria__descricao", lookup_expr="exact")
@@ -15,8 +15,11 @@ class LivroFilter(FilterSet):
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = LivroFilter
+    search_fields = ["titulo"]
+    ordering_fields = ["titulo", "preco"]
+    ordering = ["titulo"]
 
     def get_serializer_class(self):
         if self.action == "list":
