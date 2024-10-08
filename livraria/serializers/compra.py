e1a6b0256da1fedeb8df2c294b8c9796d2aac181
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 07:47:49 -0300

    feat: inluindo visualização dos itens da compra no endpoint de compras

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,5 +1,13 @@
 from rest_framework.serializers import ModelSerializer, CharField
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta: 
+        model = ItensCompra
+        fields = ["livro", "quantidade"]
+        depth = 2   # depth=2 inlui detalhes dos relacionamentos, aumentando a profundidade da serialização.
+                    # Se depth=1 fosse usado incluiria os detalhes dos relacionamentos diretos (pk's).
 
 class CompraSerializer(ModelSerializer):
     
@@ -7,7 +15,8 @@ class CompraSerializer(ModelSerializer):
     # O parâmetro read_only indica que o campo em questão não será utilizado para atualizar a model.
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
-    
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
         fields = "__all__"
