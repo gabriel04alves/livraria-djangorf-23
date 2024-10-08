325856a6e47186797d129ada069e86426988dc03
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-08 14:12:12 -0300

    feat: filtrando compras a partir do tipo do usuario autenticado

--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -10,3 +10,11 @@ class CompraViewSet(ModelViewSet):
         if self.action == "create" or self.action == "update":
             return CriarEditarCompraSerializer
         return CompraSerializer
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
